8816023f83f8664eb6b7b5fad7d8df239a7f6d3a
Nicolas Delgado <32339920+nadelgadoc@users.noreply.github.com>
2020-07-22 15:05:39 -0400

    Update readMifare.py

--- examples/readMifare.py
+++ examples/readMifare.py
@@ -67,6 +67,8 @@ def loop():
     #  'uid' will be populated with the UID, and uidLength will indicate
     #  if the uid is 4 bytes (Mifare Classic) or 7 bytes (Mifare Ultralight)
     success, uid = nfc.readPassiveTargetID(pn532.PN532_MIFARE_ISO14443A_106KBPS)
+    print(success)
+    print(uid)
 
     if (success):
         #  Display some basic information about the card
